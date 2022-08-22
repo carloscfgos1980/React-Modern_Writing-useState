d40625f83fb9ea8f508913e8fd2b3821da2300cc
CarlosCubNed <97036823+carloscfgos1980@users.noreply.github.com>
2022-08-22 06:48:42 +0200

    Add files via upload

---
+++ useState()/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react"
+
+function App() {
+    const [answer] = useState("Yes")
+    return (
+        <div>
+            <h1>Is state important to know? {answer}</h1>
+        </div>
+    )
+}
+//This way is a lot simplier and it is not need the CLASS COMPONENT LIKE BELLOW
+
+// class App extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             answer: "Yes"
+//         }
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Is state important to know? {this.state.answer}</h1>
+//             </div>
+//         )
+//     }
+// }
+
+export default App
